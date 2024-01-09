ced8669be5e1b57b26471809f4aba206d59a349f
tyanochku_by <ramyud@st.amu.edu.pl>
2024-01-09 18:33:30 +0300

    alpha

---
+++ run.py
@@ -0,0 +1,119 @@
+import pandas as pd 
+import torch
+from torch import nn
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+raw_data = pd.read_csv('gender-classifier-DFE-791531.csv')
+
+y = raw_data['gender'].apply(
+    lambda gender: 0 if gender == 'male' else 1
+)
+tweets = raw_data['text'].to_numpy()
+data = pd.DataFrame({
+    'X': raw_data['text'],
+    'y': raw_data['gender']
+})
+tfidf = TfidfVectorizer(
+    ngram_range=(1,3),
+    stop_words = 'polish',
+    max_features=2000
+)
+X = tfidf.fit_transform(tweets)
+print(f'X shape: {X.shape}')
+
+X = torch.from_numpy(X).type(torch.float)
+y = torch.from_numpy(y).type(torch.float)
+
+# Split data into train and test sets
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) # make the random split reproducible
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+
+class ModelClass(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features=2, out_features=10)
+        self.layer_2 = nn.Linear(in_features=10, out_features=10)
+        self.layer_3 = nn.Linear(in_features=10, out_features=1)
+        self.relu = nn.ReLU() # <- add in ReLU activation function
+        # Can also put sigmoid in the model 
+        # This would mean you don't need to use it on the predictions
+        # self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+      # Intersperse the ReLU activation function between layers
+       return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
+
+model_0 = ModelClass().to(device)
+print(model_0)
+
+untrained_preds = model_0(X_test.to(device))
+print(f"Length of predictions: {len(untrained_preds)}, Shape: {untrained_preds.shape}")
+print(f"Length of test samples: {len(y_test)}, Shape: {y_test.shape}")
+print(f"\nFirst 10 predictions:\n{untrained_preds[:10]}")
+print(f"\nFirst 10 test labels:\n{y_test[:10]}")
+
+# Create a loss function
+# loss_fn = nn.BCELoss() # BCELoss = no sigmoid built-in
+loss_fn = nn.BCEWithLogitsLoss() # BCEWithLogitsLoss = sigmoid built-in
+
+# Create an optimizer
+optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
+
+torch.manual_seed(42)
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    return (correct / len(y_pred)) * 100
+
+
+epochs = 100
+
+# Put data to target device
+X_train, y_train = X_train.to(device), y_train.to(device)
+X_test, y_test = X_test.to(device), y_test.to(device)
+
+# Build training and evaluation loop
+for epoch in range(epochs):
+    ### Training
+    model_0.train()
+
+    # 1. Forward pass (model outputs raw logits)
+    y_logits = model_0(X_train).squeeze() # squeeze to remove extra `1` dimensions, this won't work unless model and data are on same device 
+    y_pred = torch.round(torch.sigmoid(y_logits)) # turn logits -> pred probs -> pred labls
+  
+    # 2. Calculate loss/accuracy
+    # loss = loss_fn(torch.sigmoid(y_logits), # Using nn.BCELoss you need torch.sigmoid()
+    #                y_train) 
+    loss = loss_fn(y_logits, # Using nn.BCEWithLogitsLoss works with raw logits
+                   y_train) 
+    acc = accuracy_fn(y_true=y_train, 
+                      y_pred=y_pred) 
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backwards
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    ### Testing
+    model_0.eval()
+    with torch.inference_mode():
+        # 1. Forward pass
+        test_logits = model_0(X_test).squeeze() 
+        test_pred = torch.round(torch.sigmoid(test_logits))
+        # 2. Caculate loss/accuracy
+        test_loss = loss_fn(test_logits,
+                            y_test)
+        test_acc = accuracy_fn(y_true=y_test,
+                               y_pred=test_pred)
+
+    # Print out what's happening every 10 epochs
+    if epoch % 10 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
